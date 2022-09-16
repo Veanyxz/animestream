6ac2c442a911fcd92929877d91b1a21a02746041
Kishor Krishna <kishorkrishna3k@gmail.com>
2022-09-17 00:02:37 +0530

    completly changing card breakpoints

--- client/src/components/Layouts/CarouselRenderer.jsx
+++ client/src/components/Layouts/CarouselRenderer.jsx
@@ -16,11 +16,11 @@ export default function CarouselRenderer({
   const breakPoints = [
     { width: 1, itemsToShow: isUpcoming ? 2 : 3 },
 
-    { width: 670, itemsToShow: isUpcoming ? 3 : 3 },
+    { width: 600, itemsToShow: isUpcoming ? 2 : 4 },
+    { width: 800, itemsToShow: isUpcoming ? 3 : 4 },
+    { width: 900, itemsToShow: isUpcoming ? 4 : 5 },
 
-    { width: 1100, itemsToShow: isUpcoming ? 4 : 4 },
-
-    { width: 1580, itemsToShow: isUpcoming ? 4 : 4 },
+    { width: 1440, itemsToShow: isUpcoming ? 4 : 6 },
     { width: 1760, itemsToShow: isUpcoming ? 4 : 7 },
     { width: 1920, itemsToShow: isUpcoming ? 4 : 8 },
   ];
